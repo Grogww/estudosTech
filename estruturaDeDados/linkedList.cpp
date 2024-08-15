d84fb5da1157f849af96c30ce582edbadfae3390
Eduardo Fritsch Silva <eduardofritschsilva@gmail.com>
2024-08-15 20:48:17 -0300

    finalização linkedList Estrutura de Dados

--- estruturaDeDados/linkedList.cpp
+++ estruturaDeDados/linkedList.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 //Definição do nó da lista
 typedef struct node{
 	int data;
@@ -17,23 +18,52 @@ typedef struct list{
 //function p/ criar um novo nó
 node* createNode(int data){
 	node* newNode = (node*) malloc(sizeof(node));
-	if(newNode == null){
+	if(newNode == NULL){
 		printf("Erro ao alocar memória.\n");
 		return newNode;
 	}
 	newNode->data = data;
-	newNode->next = null;
+	newNode->next = NULL;
 	return newNode;
 }
 
 void initializeList(list* list){
-	list->head = null;
-	list->tail = null;
+	list->head = NULL;
+	list->tail = NULL;
 }
 
+void insertNode(list* list, int data){
+	node* newNode = createNode(data);
+	if(list->head == NULL){
+		list->head = newNode;
+	}else{
+		node* current = list->head;
+		while(current->next != NULL){
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+}
 
+void displayList(list* list){
+	node* current = list->head;
+	while(current != NULL){
+		printf("%i -> ", current->data);
+		current = current->next;
+	}
+	printf("NULL\n");
+}
 
-
-
+main(){
+	list list;
+	initializeList(&list);
+	
+	insertNode(&list, 10);
+	insertNode(&list, 20);
+	insertNode(&list, 30);
+	
+	printf("Lista encadeada: ");
+	displayList(&list);
+}
 
 
